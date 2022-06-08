d63cade7d8e21a69eed1d49bd39342fe932a2eb2
Pavel Kozlov <51158902+PavelKozlov1983@users.noreply.github.com>
2022-06-08 10:59:45 +0300

    Update curr_to_doll.cpp

--- curr_to_doll.cpp
+++ curr_to_doll.cpp
@@ -6,23 +6,40 @@ int main()
     constexpr double rur = 0.016405;
     constexpr double cny = 0.15257;
     constexpr double byn = 0.38402;
+    constexpr double eur = 1.07;
+    constexpr double gpb = 1.26;
     double cash = 1.0;
     char unit = ' ';
     cout << "Введите сумму и валюту(r, c ,b, d)\n";
     cin >> cash >> unit;
-    if (unit == 'r')
-        cout << cash <<" руб. == " <<
-        cash*rur << " долл.\n";
-    else if (unit == 'c')
-        cout << cash <<" юан. == " <<
-        cash*cny << " долл.\n";
-    else if (unit == 'b')
-        cout << cash <<" блр. == " <<
-        cash*byn << " долл.\n";
-    else if (unit == 'd')
-        cout << cash << " долл.\n";
-    else
-        cout << "Я не знаю, что такое '" << unit << "'.\n";
+    switch (unit){
+        case 'r':
+            cout << cash <<" руб. == " <<
+            cash*rur << " долл.\n";
+            break;
+        case 'c':
+            cout << cash <<" юан. == " <<
+            cash*cny << " долл.\n";
+            break;
+        case 'b':
+            cout << cash <<" блр. == " <<
+            cash*byn << " долл.\n";
+            break;
+        case 'd':
+            cout << cash << " долл.\n";
+            break;
+        case 'e':
+            cout << cash <<" евро == " <<
+            cash*eur << " долл.\n";
+            break;
+        case 'g':
+            cout << cash <<" фунт. == " <<
+            cash*gpb << " долл.\n";
+            break;
+        default:
+            cout << "Я не знаю, что такое '" << unit << "'.\n";
+            break;
+    }
     
     return 0;
 }
