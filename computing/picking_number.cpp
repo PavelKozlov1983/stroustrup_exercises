dc853cb4ad0088369da993500578ca5a8b35dbe6
Pavel Kozlov <51158902+PavelKozlov1983@users.noreply.github.com>
2022-11-03 17:40:41 +0300

    Update picking_number.cpp

--- computing/picking_number.cpp
+++ computing/picking_number.cpp
@@ -1,37 +1,37 @@
-#include "std_lib_facilities.h"
+//#include "std_lib_facilities.h"
 
 int main()
 {
-    int picked_num = 100;
+    int picked_num = 50;
     char users_answer = ' ';
-    int difference = 100;
+    int difference = 50;
     int counter = 0;
-    cout << "Я буду угадывать число, а вы отвечайте:"
+    std::cout << "Я буду угадывать число, а вы отвечайте:"
     << " + больше , - меньше, ! угадал\n"
     << "и нажимайте ввод.\n"
     << "Загадайте число от 0 до 100\n";
     while (users_answer != '!') {
-        cout << "Это число  " << picked_num
+    	std::cout << "Это число  " << picked_num
         <<" ?\n";
-        cin >> users_answer;
+    	std::cin >> users_answer;
         switch (users_answer) {
             case '+':
-                difference /= 2;
+            	difference > 1 ? difference /= 2 : difference;
                 picked_num += difference;
                 break;
             case '-':
-                difference /= 2;
+                difference > 1 ? difference /= 2 : difference;
                 picked_num -= difference;
                 break;
             case '!':
                 break;
             default:
-                cout << "Вы ввели не тот символ!\n";
+            	std::cout << "Вы ввели не тот символ!\n";
                 break;
         }
         ++counter;
     }
-    cout << "Ура, это число " << picked_num << "!\n";
-    cout << "Угадал за " << counter << " ходов!\n";
+    std::cout << "Ура, это число " << picked_num << "!\n";
+    std::cout << "Угадал за " << counter << " ходов!\n";
     return 0;
 }
