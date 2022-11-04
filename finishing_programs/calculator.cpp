b8ee98699ced1a587895c23f29eda9c57fd2d2fa
Pavel Kozlov <51158902+PavelKozlov1983@users.noreply.github.com>
2022-11-04 14:46:41 +0300

    Update calculator.cpp

--- finishing_programs/calculator.cpp
+++ finishing_programs/calculator.cpp
@@ -1,51 +1,62 @@
-/*
-	calculator08buggy.cpp
-
-	Helpful comments removed.
-
-	We have inserted 3 bugs that the compiler will catch and 3 that it won't.
-*/
-
-
 #include "std_lib_facilities.h"
 
 //------------------------------------------------------------------------------
 
-
 struct Token {
-	char kind;
-	double value;
-	string name;
-	Token(char ch) :kind(ch), value(0) { }
-	Token(char ch, double val) :kind(ch), value(val) { }
-	Token(char ch, string s) :kind(ch), name(s), value(0) { }
+	char kind;  // what kind of token
+	double value;  // for numbers: a value
+	string name;   // for name of variables
+	Token(char ch)
+		:kind(ch), value(0) { }  // make a Token from a char
+	Token(char ch, double val)
+		:kind(ch), value(val) { }  // make a Token from a char and a double
+	Token(char ch, string s)
+		:kind(ch), name(s){ }  // make a Token from a char and a string
+
 };
 
 class Token_stream {
 public:
-	Token_stream() :full(0), buffer(0) { }
-	Token get();
-	void unget(Token t) { buffer = t; full = true; }
+	Token_stream() :full(0), buffer(0) { } // make a Token_stream that reads from cin
+	Token get();  // get a Token (get() is defined elsewhere)
+	void unget(Token t) { buffer = t; full = true; }  // put a Token back
 	void ignore(char);
 private:
-	bool full;
-	Token buffer;
+	bool full; // is there a Token in the buffer?
+	Token buffer; // here is where we keep a Token put back using unget()
 };
 
-const char let = 'L';
-const char quit = 'Q';
-const char print = ';';
-const char number = '8';
-const char name = 'a';
+//------------------------------------------------------------------------------
+
+const char let = 'L';  // t.kind == let for var definition
+const char quit = '#'; // t.kind == quit - exit token
+const char print = ';';  // t.kind == print - print token
+const char number = '8'; // t.kind == number - means number
+const char name = 'A';   // t.kind == name - means var name
+const char square_root = 'S'; // t.kind == sqare_root
+const char exponentiation = 'P'; // t.kind == exponentiation
+const char const_setting = 'C';
+const string declkey = "let"; // declaration key word
+const string sq_root_key = "sqrt"; // declaration of square root command
+const string exponent_key = "pow";
+
+
+//------------------------------------------------------------------------------
 
 Token Token_stream::get()
 {
-	if (full) { full = false; return buffer; }
-	char ch;
+	if (full) // do we already have a Token ready?
+		{ full = false; return buffer; } //remove token from buffer
+	char ch; // note that >> skips whitespace (space, newline, tab, etc.)
 	cin >> ch;
 	switch (ch) {
 	case print:
 	case quit:
+	case square_root:
+	case exponentiation:
+	case '{':
+	case '}':
+	case ',':
 	case '(':
 	case ')':
 	case '+':
@@ -54,8 +65,8 @@ Token Token_stream::get()
 	case '/':
 	case '%':
 	case '=':
-		return Token(ch);
-	case '.':
+		return Token(ch); // let each character represent itself
+	case '.':  // floating num can starts from .
 	case '0':
 	case '1':
 	case '2':
@@ -66,24 +77,33 @@ Token Token_stream::get()
 	case '7':
 	case '8':
 	case '9':
-	{	cin.unget();
+	{	cin.unget(); // put digit back into the input stream
 	double val;
-	cin >> val;
+	cin >> val; // read a floating-point number
 	return Token(number, val);
 	}
 	default:
 		if (isalpha(ch)) {
+			bool is_a_const;
 			string s;
 			s += ch;
-			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s = ch;
-			cin.unget();
-			if (s == "let") return Token(let);
-			if (s == "quit") return Token(name);
-			return Token(name, s);
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch) || ch == '_'))
+				s += ch;
+				cin.unget();
+				if (s == declkey)
+					return Token(let);
+				else if(s == sq_root_key)
+					return Token(square_root);
+				else if (s == exponent_key)
+					return Token(exponentiation);
+
+				return Token(name, s);
 		}
 		error("Bad token");
 	}
 }
+
+//------------------------------------------------------------------------------
 
 void Token_stream::ignore(char c)
 {
@@ -93,31 +113,60 @@ void Token_stream::ignore(char c)
 	}
 	full = false;
 
-	char ch;
+	char ch = 0;
 	while (cin >> ch)
 		if (ch == c) return;
 }
 
-struct Variable {
+//------------------------------------------------------------------------------
+
+Token_stream ts;
+
+//------------------------------------------------------------------------------
+
+double expression();
+
+//------------------------------------------------------------------------------
+
+double term();
+
+//------------------------------------------------------------------------------
+
+double primary();
+
+//------------------------------------------------------------------------------
+
+class Variable {
+public:
 	string name;
 	double value;
-	Variable(string n, double v) :name(n), value(v) { }
-};
+	
+	Variable(string n, double v)
+		:name(n), value(v) { } //for non const variables
 
-vector<Variable> names;
+};
+//------------------------------------------------------------------------------
+
+
+
+//------------------------------------------------------------------------------
+
+vector<Variable> var_table;
+
+//------------------------------------------------------------------------------
 
 double get_value(string s)
 {
-	for (int i = 0; i < names.size(); ++i)
-		if (names[i].name == s) return names[i].value;
+	for (const Variable& v : var_table)
+		if (v.name == s) return v.value;
 	error("get: undefined name ", s);
 }
 
 void set_value(string s, double d)
 {
-	for (int i = 0; i <= names.size(); ++i)
-		if (names[i].name == s) {
-			names[i].value = d;
+	for (Variable v : var_table)
+		if (v.name == s) {
+			v.value = d;
 			return;
 		}
 	error("set: undefined name ", s);
@@ -125,34 +174,106 @@ void set_value(string s, double d)
 
 bool is_declared(string s)
 {
-	for (int i = 0; i < names.size(); ++i)
-		if (names[i].name == s) return true;
+	for (const Variable& v : var_table)
+		if (v.name == s) return true;
 	return false;
 }
 
-Token_stream ts;
+//------------------------------------------------------------------------------
 
-double expression();
+double define_name(string var, double val)
+{
+	if (is_declared(var)) error(var, " declared twice");
+	var_table.push_back(Variable(var, val));
+}
+//------------------------------------------------------------------------------
+
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != name) error("name expected in declaration");
+	string var_name = t.name;
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of ", name);
+	double d = expression();
+	define_name(var_name, d);
+	return d;
+}
+
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind) {
+	case let:
+		return declaration();
+	default:
+		ts.unget(t);
+		return expression();
+	}
+}
+
+//------------------------------------------------------------------------------
 
 double primary()
 {
 	Token t = ts.get();
 	switch (t.kind) {
+	case exponentiation:
+	{
+		t = ts.get();
+		if (t.kind != '{') error("'{' expected");
+
+		double d = expression(); // num
+		t = ts.get();
+		if (t.kind != ',') error("',' expected");
+		int i = narrow_cast<int>(expression()); // exponent
+		t = ts.get();
+		if (t.kind != '}') error("'}' expected");
+		return pow(d, i);
+	}
+	case square_root:
+	{
+		double d = expression();
+			if(d < 0) error("taking the square root of a negative");
+		return sqrt(d);
+	}
 	case '(':
-	{	double d = expression();
-	t = ts.get();
-	if (t.kind != ')') error("'(' expected");
+	{
+		double d = expression();
+		t = ts.get();
+		if (t.kind != ')') error("'(' expected");
+		return d;
 	}
 	case '-':
 		return -primary();
+	case '+':
+	    	return primary();
 	case number:
 		return t.value;
-	case name:
-		return get_value(t.name);
+	case name: {
+		Token next = ts.get();
+		if (next.kind == '=') // handle name = expression
+			{
+				double d = expression();
+				set_value(t.name, d);
+				return d;
+			}
+		else
+		{
+			ts.unget(next);
+			return get_value(t.name);
+		}
+	}
 	default:
 		error("primary expected");
 	}
 }
+
+//------------------------------------------------------------------------------
 
 double term()
 {
@@ -169,12 +290,22 @@ double term()
 		left /= d;
 		break;
 		}
+		case '%':
+		{
+			double d = primary();
+		    if (d == 0) error("divide by zero");
+		    left = fmod(left, d);
+		    t = ts.get();
+		    break;
+		}
 		default:
 			ts.unget(t);
 			return left;
 		}
 	}
 }
+
+//------------------------------------------------------------------------------
 
 double expression()
 {
@@ -195,38 +326,19 @@ double expression()
 	}
 }
 
-double declaration()
-{
-	Token t = ts.get();
-	if (t.kind != 'a') error("name expected in declaration");
-	string name = t.name;
-	if (is_declared(name)) error(name, " declared twice");
-	Token t2 = ts.get();
-	if (t2.kind != '=') error("= missing in declaration of ", name);
-	double d = expression();
-	names.push_back(Variable(name, d));
-	return d;
-}
 
-double statement()
-{
-	Token t = ts.get();
-	switch (t.kind) {
-	case let:
-		return declaration();
-	default:
-		ts.unget(t);
-		return expression();
-	}
-}
 
 void clean_up_mess()
 {
 	ts.ignore(print);
 }
 
-const string prompt = "> ";
-const string result = "= ";
+//------------------------------------------------------------------------------
+
+const string prompt = "> "; // input prompt
+const string result = "= "; // indicate that follows is a result
+
+//------------------------------------------------------------------------------
 
 void calculate()
 {
@@ -244,9 +356,20 @@ void calculate()
 	}
 }
 
+//------------------------------------------------------------------------------
+
 int main()
 
 try {
+
+	define_name("pi", 3.1415926535);
+	define_name("e", 2.7182818284);
+	define_name("k", 1000);
+
+	 cout << "Welcome to calculator!\n"
+	    <<"Please enter expression. Use digits, ., +, -, *, = /\n";
+
+
 	calculate();
 	return 0;
 }
